02b88c5f4f760ac48aa098cfd55d5443a6c8fb93
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-25 10:04:56 -0400

    part 2,3,4

--- regexor.rb
+++ regexor.rb
@@ -1,3 +1,13 @@
+
+# def exactly_three_chars(animals)
+#   matches = []
+#   animals.each do |animal|
+#     if animal.match(/^[a-z]{3}$/i)   #i signifies case-insensitive
+#       matches << animal
+#     end
+#   end
+#   matches
+# end
 # class Regexor
 #   class << self
 #     def filter_png(files)
@@ -17,6 +27,24 @@ class Regexor
       end
       matches
     end
+
+    def exactly_three_chars(animals)
+      animals.delete_if do |animal|
+        !animal.match(/^[a-z]{3}$/i)
+      end
+    end
+
+    def does_not_start_with_b(names)
+      names.delete_if do |name|
+        name.match(/^b/i)
+      end
+    end
+
+    def no_bees(names)
+      names.delete_if do |name|
+        name.match(/b/i)
+      end
+    end
   end
 end
 
@@ -25,5 +53,20 @@ class TestRegexor < Minitest::Test
   def test_filter_png
     files = %w{file1.jpg file2.png file3.gif}
     assert_equal %w{file2.png}, Regexor.filter_png(files)
+  end
+
+  def text_exactly_three_chars
+    animals = %w{pig bear lion}
+    assert_equal %w{Pig}, Regexor.exactly_three_chars(animals)
+  end
+
+  def test_does_not_start_with_b
+    names = %w{Barbara Abby Margarita}
+    assert_equal %w{Abby Margarita}, Regexor.does_not_start_with_b(names)
+  end
+
+  def method_name
+    names = %w{Barbara Abby Margarita}
+    assert_equal %w{Margarita}, Regexor.no_bees(names)
   end
 end
